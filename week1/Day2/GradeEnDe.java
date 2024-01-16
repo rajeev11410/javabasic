ccee72a3ee0190f6d19873ba9ebe119bc5005968
rajeev11410 <85879771+rajeev11410@users.noreply.github.com>
2024-01-16 22:24:13 +0530

    Create GradeEnDe.java

---
+++ week1/Day2/GradeEnDe.java
@@ -0,0 +1,13 @@
+class GradeEnDe {
+  public static void main(String[] args){
+    char grade='A';
+    grade=(char)(grade+8);
+    //after encryption
+    System.out.println(grade);
+
+    //after decryption
+    grade=(char)(grade-8);
+    System.out.println(grade);
+    
+  }
+}
