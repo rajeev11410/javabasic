88afa64198419990c83347d0a38e7f93d9127d8d
rajeev11410 <85879771+rajeev11410@users.noreply.github.com>
2024-01-16 18:38:17 +0530

    Update tsk1.java

--- tsk1.java
+++ tsk1.java
@@ -1,1 +1,16 @@
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
 
+      int sub1=sc.nextInt();
+      int sub2=sc.nextInt();
+      int sub3=sc.nextInt();
+      int sub4=sc.nextInt();
+      int sub5=sc.nextInt();
+
+      int totalmarks=(sub1+sub2+sub3+sub3+sub4+sub5);
+         Double percentage= ((double)totalmarks/500)*100;
+         System.out.println("percentage:"+percentage);
+    }
+}
