f7a5dcf883b296070de4e229bf2166f3289f2213
rajeev11410 <85879771+rajeev11410@users.noreply.github.com>
2024-01-28 12:57:34 +0530

    Update Modifier.java

--- week2./Day5/Modifier.java
+++ week2./Day5/Modifier.java
@@ -3,7 +3,7 @@ class Cylinder{
   int radius;
   int height;
   Cylinder(int radius,int height){
-    this.raadius=radius;
+    this.radius=radius;
     this.height=height;
   }
   
@@ -31,7 +31,39 @@ class Cylinder{
 }
 
 class Rectangle{
-  
+     int length;
+     int breadth;
+  Rectangle(int l,int b){
+    this.length=l;
+    this.breadth=b;
+  }
+  int getLength(){
+    return length;
+  }
+  int getBreadth(){
+    return breadth;
+  }
+  void setLength(int l){
+    this.length=l;
+  }
+  void setBreadth(int b){
+    this.breadth=b;
+  }
+  int area(int l,int b){
+    return l*b;
+  }
+}
+class Modifier{
+  public static void main(String[] args){
+    Cylinder c=new Cylinder(2,3);
+    Rectangle r=new Rectangle(4,5);
+    c.setRadius(5);
+    c.setHeight(10);
+    System.out.println("volume of Cylinder is:"+c.volume(10,5));
+    System.out.println("surface area:"+c.surfaceArea(12,5));
+    System.out.println("area of rectangler:"+r.area(10,20));
+     System.out.println("area of rectangler:"+r.getBreadth());
+  }
 }
 
 
